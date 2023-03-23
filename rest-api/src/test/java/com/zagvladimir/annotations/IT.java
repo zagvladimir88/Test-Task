7cf684aa7e6fb634e7525a7d9d93c27992e20a6a
zagvladimir88 <zagvladimir88@gmail.com>
2023-03-23 17:14:38 +0300

    Integration tests

---
+++ rest-api/src/test/java/com/zagvladimir/annotations/IT.java
@@ -0,0 +1,21 @@
+package com.zagvladimir.annotations;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@TestPropertySource(properties = {"spring.config.location = classpath:application-test.yml"})
+public @interface IT {
+}
